ad9aca0ca0cd89586c5c0e386cfb4e154ea8baba
Maurochz <analista.maurochz@gmail.com>
2023-05-15 00:10:35 -0300

    retoques

--- ChallengeCRUD/Models/Empleado.cs
+++ ChallengeCRUD/Models/Empleado.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ChallengeCRUD.Models;
 
 public partial class Empleado
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre es requerido.")]
     public string Nombrecompleto { get; set; } = null!;
-
+    [Required(ErrorMessage = "La edad es requerida.")]
+    [Range(0, 150, ErrorMessage = "La edad debe estar entre 0 y 150 años.")]
     public short Edad { get; set; }
-
-    public decimal Salario { get; set; }
-
+   
+   
+    [Required(ErrorMessage = "El salario es requerido.")]
+    [Column("Salario", TypeName = "float")]
+    public double Salario { get; set; }
+    [Required(ErrorMessage = "El departamento es requerido.")]
     public string Departamento { get; set; } = null!;
 }
